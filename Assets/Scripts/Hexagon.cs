abbc4687b65cb9b4da2968442ab0a35fa8ac5c58
SjoerdJakobs <18284@ma-web.nl>
2016-09-26 16:27:43 +0200

    hexasync
    hexasync

---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class Hexagon
+{
+
+    public const float radius = 10f;
+
+    public const float innerRadius = radius * 0.866025404f;
+
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, radius),
+        new Vector3(innerRadius, 0f, 0.5f * radius),
+        new Vector3(innerRadius, 0f, -0.5f * radius),
+        new Vector3(0f, 0f, -radius),
+        new Vector3(-innerRadius, 0f, -0.5f * radius),
+        new Vector3(-innerRadius, 0f, 0.5f * radius),
+        new Vector3(0f, 0f, radius)
+    };
+}
