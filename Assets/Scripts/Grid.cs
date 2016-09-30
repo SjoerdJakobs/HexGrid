676480bddecbf741052123d84c8565e764261ab2
SjoerdJakobs <18284@ma-web.nl>
2016-09-30 16:28:18 +0200

    after this just one sync
    after this just one sync

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,52 +1,87 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Grid : MonoBehaviour
 {
 
     public int width = 6;
     public int height = 6;
+    public float spacing = 0;
+
+    [SerializeField]
+    private bool multipleObjects;
 
     [SerializeField]
     private Material matt;
 
-    private GameObject[] hexagonArray;
+    private GameObject[,] hexagonArray;
+
+    public List<Vector3> points;
 
     void Awake()
     {
-        hexagonArray = new GameObject[width*height];
+        points = new List<Vector3>();
+        hexagonArray = new GameObject[width,height];
         createGrid();
     }
 
-    /*void Start()
+    void Start()
     {
-        Invoke("DestroyGrid", 5);
-        Invoke("createGrid", 8);
-    }*/
-
-    void createGrid()
-    {
-        for (int z = 0, i = 0; z < height; z++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                AddHexagon(x, z, i++);
-            }
-        }
+        //Invoke("DestroyGrid", 5);
+        //Invoke("createGrid", 8);
     }
 
-    void DestroyGrid()
+    public void createGrid()
     {
+        DestroyGrid();
         for (int z = 0, i = 0; z < height; z++)
         {
             for (int x = 0; x < width; x++)
             {
-                Destroy(hexagonArray[i]);
                 i++;
+                if (multipleObjects)
+                {
+                    AddHexagon(x, z);
+                }
+                else
+                {
+                    createPoints(x, z, i);
+                }
+            }
+        }
+        for (int i = 0; i < points.Count; i++)
+        {
+            //print(points[i] + "point" + i);
+        }
+    }
+
+    public void DestroyGrid()
+    {
+        for (int z = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Destroy(hexagonArray[x,z]);
             }
         }
     }
 
-    void AddHexagon(int x, int z, int i)
+    void createPoints(int x, int z, int o)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * ((Hexagon.innerRadius + spacing) * 2f);
+        position.y = 0f;
+        position.z = z * ((Hexagon.radius + spacing) * 1.5f);
+        Vector3[] tempArray = Hexagon.Corners.singleCorners;
+        for(int j = 0; j < tempArray.Length; j++)
+        {
+            tempArray[j] += position;
+            points.Add(tempArray[j]);
+            print(points.Count);
+        }
+    }
+
+    void AddHexagon(int x, int z)
     {
         GameObject hexagon = new GameObject("Hexagon");
         hexagon.AddComponent<MeshRenderer>();
@@ -54,15 +89,24 @@ public class Grid : MonoBehaviour
         rendr.material = matt;
         rendr.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
         hexagon.AddComponent<Hexagon>();
-        Hexagon hexagonInfo = hexagon.GetComponent<Hexagon>();
 
         Vector3 position;
-        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.x = (x + z * 0.5f - z / 2) * ((Hexagon.innerRadius+spacing) * 2f);
         position.y = 0f;
-        position.z = z * (HexMetrics.outerRadius * 1.5f);
+        position.z = z * ((Hexagon.radius+spacing) * 1.5f);
 
         hexagon.transform.position = position;
 
-        hexagonArray[i] = hexagon;       
+        hexagonArray[x,z] = hexagon;       
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.black;
+        for (int i = 0; i < points.Count; i++)
+        {
+            Gizmos.DrawSphere(points[i], 0.1f);
+        }
     }
 }
--- Assets/Scripts/Hexagon.cs
+++ Assets/Scripts/Hexagon.cs
@@ -5,31 +5,31 @@
 public class Hexagon : MonoBehaviour
 {
     /// <summary>
-    /// here we generate a hexagon together with its variables.
+    /// here we generate a hexagon together with its variables or even only its variables to give to another script.
     /// </summary>
     private Mesh mesh;
     private Vector3[] vertices;
     private Vector3 transf;
+    
+    public static float radius = 10f;
 
-
-    public const float radius = 10f;
-
-    public const float innerRadius = radius * 0.866025404f;
+    public static float innerRadius = radius * 0.866025404f;
 
 
     void Awake()
     {
         transf = transform.position;
-        Generate();
+        GenerateSingleHex();
     }
         
-
-    
-    public void Generate()
+    public void GenerateSingleHex()
     {
+        //here i hard code a hexagon with wals under it because it was faster and easier
+        //to do for me that some sort of loop. 
+
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "hexagon";
-        Vector3[] positionCorners = Corners.corners;
+        Vector3[] positionCorners = Corners.multipleCorners;
         for(int i = 0; i < positionCorners.Length; i++)
         {
             positionCorners[i] += transf;
@@ -37,47 +37,102 @@ public class Hexagon : MonoBehaviour
 
         mesh.vertices = positionCorners;
 
-        int[] triangles = new int[12];
+
+        int[] triangles = new int[30];
+        //right top middle triangele
         triangles[0] = 1;
         triangles[1] = 2;
         triangles[2] = 5;
 
+        //left top middle triangle
         triangles[3] = 2;
         triangles[4] = 4;
         triangles[5] = 5;
 
+        //right top triangle
         triangles[6] = 0;
         triangles[7] = 1;
         triangles[8] = 5;
 
+        //left top triangle
         triangles[9] = 2;
         triangles[10] = 3;
         triangles[11] = 4;
 
+
+        //wals
+        triangles[12] = 6;
+        triangles[13] = 0;
+        triangles[14] = 5;
+
+        triangles[15] = 11;
+        triangles[16] = 6;
+        triangles[17] = 5;
+
+        triangles[18] = 6;
+        triangles[19] = 7;
+        triangles[20] = 0;
+
+        triangles[21] = 7;
+        triangles[22] = 1;
+        triangles[23] = 0;
+
+        triangles[24] = 8;
+        triangles[25] = 2;
+        triangles[26] = 1;
+
+        triangles[27] = 7;
+        triangles[28] = 8;
+        triangles[29] = 1;
+
         mesh.triangles = triangles;
     }
-    private void OnDrawGizmos()
+
+    public struct Corners
     {
-        if (vertices == null)
-        {
-            return;
-        }
-        Gizmos.color = Color.black;
-        for (int i = 0; i < Corners.corners.Length; i++)
-        {
-            Gizmos.DrawSphere(Corners.corners[i], 0.1f);
-        }
+        /// <summary>
+        /// here i set the position of the vertices of 2 kinds of hexagons. 
+        /// the single object hexagon and the multiple objects hexagon.
+        /// while i know that this gives it more responsebility than it should have it wil do for now.
+        /// with the multipleCorners i give the vertices for a hex grid where every hex is its own object
+        /// while the singleCorners is to just give a hex form with vertices to a script that could
+        /// create its own hexgrid with just one mesh.
+        /// </summary>
+        public static Vector3[] multipleCorners = {
+            //top level
+        new Vector3(0f, 10f, radius),//corner 0, far top corner
+        new Vector3(innerRadius, 10f, 0.5f * radius),//corner 1,top right corner 
+        new Vector3(innerRadius, 10f, -0.5f * radius),//corner 2, down right corner
+        new Vector3(0f, 10f, -radius),//corner 3, fardown corner
+        new Vector3(-innerRadius, 10f, -0.5f * radius),//corner 4, down left corner
+        new Vector3(-innerRadius, 10f, 0.5f * radius),//corner 5,top left corner 
+            
+            //lower level
+        new Vector3(0f, 0f, radius),//corner 6, far top corner
+        new Vector3(innerRadius, 0f, 0.5f * radius),//corner 7,top right corner
+        new Vector3(innerRadius, 0f, -0.5f * radius),//corner 8, down right corner
+        new Vector3(0f, 0f, -radius),//corner 9, fardown corner
+        new Vector3(-innerRadius, 0f, -0.5f * radius),//corner 10, down left corner
+        new Vector3(-innerRadius, 0f, 0.5f * radius),//corner 11,top left corner 
+        };
+
+        public static Vector3[] singleCorners = {
+        new Vector3(0f, 10f, radius),//corner 0, far top corner
+        new Vector3(innerRadius, 10f, 0.5f * radius),//corner 1,top right corner 
+        new Vector3(innerRadius, 10f, -0.5f * radius),//corner 2, down right corner
+        new Vector3(0f, 10f, -radius),//corner 3, fardown corner
+        new Vector3(-innerRadius, 10f, -0.5f * radius),//corner 4, down left corner
+        new Vector3(-innerRadius, 10f, 0.5f * radius),//corner 5,top left corner 
+        };
     }
 
-    public static class Corners
+    private void OnDrawGizmos()
     {
-        public static Vector3[] corners = {
-        new Vector3(0f, 0f, radius),
-        new Vector3(innerRadius, 0f, 0.5f * radius),
-        new Vector3(innerRadius, 0f, -0.5f * radius),
-        new Vector3(0f, 0f, -radius),
-        new Vector3(-innerRadius, 0f, -0.5f * radius),
-        new Vector3(-innerRadius, 0f, 0.5f * radius),
-        };
+
+        Gizmos.color = Color.black;
+        for (int i = 0; i < Corners.multipleCorners.Length; i++)
+        {
+            Gizmos.DrawSphere(Corners.multipleCorners[i], 0.1f);
+        }
     }
 }
